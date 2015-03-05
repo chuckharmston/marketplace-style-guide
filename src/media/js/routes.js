b7fd300ada2da3f1b0bcdcd315209481c31aeb49
Chuck Harmston <chuck@chuckharmston.com>
2015-03-05 15:43:25 -0700

    Update to marketplace-core-modules 2.0.

--- src/media/js/routes.js
+++ src/media/js/routes.js
@@ -1,29 +1,15 @@
-(function() {
+define('routes',
+    ['core/router', 'routes_static'],
+    function(router, routes_static) {
 
-// Please leave quotes around keys! They're needed for Space Heater.
-var routes = window.routes = [
-    {'pattern': '^/tests$', 'view_name': 'tests'},
-    {'pattern': '^/fxa-authorize$', 'view_name': 'fxa_authorize'},
-];
+    var routes = [];
 
-define('routes', [
-    'routes_static',
-    'views/static',
-    'views/tests',
-    'views/fxa_authorize',
-], function(routes_static, static_view) {
-    for (var i = 0; i < routes.length; i++) {
-        var route = routes[i];
-        route.view = require('views/' + route.view_name);
-    }
     Object.keys(routes_static).forEach(function(key) {
         routes.push({
             pattern: '^' + routes_static[key].path + '$',
-            view_name: 'static',
-            view: static_view,
+            view_name: 'static'
         });
     });
-    return routes;
-});
 
-})();
+    router.addRoutes(routes);
+});
